9a903f41f3f588523498cd4b3060ca696fe70135
Yash <155367011+rehaneditzzz@users.noreply.github.com>
2025-07-14 14:15:43 +0530

    04: payload integration

---
+++ src/collections/Categories.ts
@@ -0,0 +1,16 @@
+import { CollectionConfig } from "payload";
+
+export const Categories: CollectionConfig = {
+  slug: "categories",
+  access : {
+    create: () => false,
+    update: ()=> false
+  },
+  fields: [
+    {
+      name: "name",
+      type: "text",
+      required: true,
+    },
+  ],
+};
